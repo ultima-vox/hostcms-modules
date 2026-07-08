e1db183088f3e63d8e4f14ba9c6c70a8af94482d
ultima-vox <rz300@ya.ru>
2026-07-08 10:55:01 +0300

    Add Russian i18n for HTML optimizer

---
+++ optimize/optimize_repo/modules/optimize/i18n/ru.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'model_name' => 'HTML-оптимизатор',
+    'menu_name' => 'HTML-оптимизатор',
+    'settings_tab' => 'Настройки',
+    'minify_html' => 'Минифицировать HTML',
+    'combine_css' => 'Объединять и минифицировать локальные CSS-файлы',
+    'combine_js' => 'Объединять локальные JS-файлы',
+    'stats_total' => 'Всего',
+    'stats_css' => 'CSS',
+    'stats_js' => 'JS',
+    'stats_requests' => 'Запросов убрано',
+    'stats_note' => 'Статистика обновляется только при реальной пересборке CSS/JS-бандла.',
+    'invalid_setting' => 'Недопустимый параметр'
+);
