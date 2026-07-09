f168052ad3515ec2e2caed8379ec99fbfe4e4756
ultima-vox <rz300@ya.ru>
2026-07-09 13:26:37 +0300

    Add i18n/ru.php for page_optimizer module

---
+++ modules/page_optimizer/i18n/ru.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+    'menu_name' => 'Page Optimizer',
+    'title' => 'Оптимизация страниц',
+
+    'tab_main' => 'Основные настройки',
+
+    // Настройки
+    'minify_html' => 'Минифицировать HTML',
+    'combine_css' => 'Объединять CSS в один файл',
+    'minify_css' => 'Минифицировать CSS',
+    'combine_js' => 'Объединять JavaScript в один файл',
+    'minify_js' => 'Минифицировать JavaScript',
+
+    'lazy_load_images' => 'Ленивая загрузка изображений (loading="lazy")',
+    'rewrite_avif' => 'Переписывать изображения на AVIF (если файл существует)',
+    'rewrite_webp' => 'Переписывать изображения на WebP (если файл существует)',
+
+    'dns_prefetch_enabled' => 'Включить DNS Prefetch',
+    'preconnect_enabled' => 'Включить Preconnect',
+    'preload_fonts_enabled' => 'Предзагружать шрифты (preload)',
+    'critical_css_enabled' => 'Включить Critical CSS',
+
+    // Статистика
+    'stats' => 'Статистика экономии',
+    'total_saved' => 'Всего сэкономлено',
+
+    // Сообщения
+    'messages_success_save' => 'Настройки успешно сохранены',
+);
