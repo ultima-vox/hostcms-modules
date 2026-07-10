958bc83282d73e6a45269b8f0fd2f1e00ade37d6
ultima-vox <rz300@ya.ru>
2026-07-10 08:21:54 +0300

    Explicitly load page optimizer admin classes

--- admin/page_optimizer/index.php
+++ admin/page_optimizer/index.php
@@ -7,5 +7,8 @@ require_once('../../bootstrap.php');
 
 Core_Auth::authorization($sModule = 'page_optimizer');
 
+require_once CMS_FOLDER . 'modules/page_optimizer/PageOptimizer_Settings.php';
+require_once CMS_FOLDER . 'modules/page_optimizer/PageOptimizer_Controller_Index.php';
+
 $oController = new PageOptimizer_Controller_Index();
 $oController->execute();
