f3c48f57e1969460701332b781d72aac8a310bd0
ultima-vox <rz300@ya.ru>
2026-07-10 08:07:52 +0300

    Add administration translations for page optimizer

--- modules/page_optimizer/i18n/ru.php
+++ modules/page_optimizer/i18n/ru.php
@@ -3,29 +3,41 @@
 return array(
     'menu_name' => 'Page Optimizer',
     'title' => 'Оптимизация страниц',
+    'tab_main' => 'Настройки оптимизации',
 
-    'tab_main' => 'Основные настройки',
+    'status_mode' => 'Режим работы',
+    'status_enabled' => 'Включено функций',
+    'safe_mode' => 'Безопасный',
+    'custom_mode' => 'Пользовательский',
+    'safe_mode_notice' => 'По умолчанию все преобразования отключены. Включайте функции по одной и проверяйте основные типы страниц сайта.',
+    'experimental' => 'Экспериментально',
+    'head_optimization' => 'Оптимизация HEAD',
 
-    // Настройки
     'minify_html' => 'Минифицировать HTML',
+    'html_remove_comments' => 'Удалять безопасные HTML-комментарии',
     'combine_css' => 'Объединять CSS в один файл',
     'minify_css' => 'Минифицировать CSS',
     'combine_js' => 'Объединять JavaScript в один файл',
     'minify_js' => 'Минифицировать JavaScript',
 
-    'lazy_load_images' => 'Ленивая загрузка изображений (loading="lazy")',
-    'rewrite_avif' => 'Переписывать изображения на AVIF (если файл существует)',
-    'rewrite_webp' => 'Переписывать изображения на WebP (если файл существует)',
+    'lazy_load_images' => 'Добавлять loading="lazy" изображениям',
+    'rewrite_avif' => 'Подменять изображения на AVIF, если файл существует',
+    'rewrite_webp' => 'Подменять изображения на WebP, если файл существует',
 
     'dns_prefetch_enabled' => 'Включить DNS Prefetch',
+    'dns_prefetch' => 'Домены DNS Prefetch, по одному в строке',
     'preconnect_enabled' => 'Включить Preconnect',
-    'preload_fonts_enabled' => 'Предзагружать шрифты (preload)',
+    'preconnect' => 'Домены Preconnect, по одному в строке',
+    'preload_fonts_enabled' => 'Предзагружать шрифты',
+    'preload_fonts' => 'URL шрифтов, по одному в строке',
     'critical_css_enabled' => 'Включить Critical CSS',
+    'critical_css' => 'Critical CSS',
 
-    // Статистика
     'stats' => 'Статистика экономии',
-    'total_saved' => 'Всего сэкономлено',
+    'total_saved' => 'Сэкономлено данных',
+    'requests_saved' => 'Сэкономлено запросов',
 
-    // Сообщения
-    'messages_success_save' => 'Настройки успешно сохранены',
+    'save' => 'Сохранить настройки',
+    'messages_success_save' => 'Настройки успешно сохранены.',
+    'csrf_error' => 'Не удалось проверить защитный токен. Обновите страницу и повторите сохранение.',
 );
