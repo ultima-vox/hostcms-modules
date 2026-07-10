b1ca6252224afb01baf6ac7de0fa74278cef5bd8
ultima-vox <rz300@ya.ru>
2026-07-10 12:57:30 +0300

    Bump Optimizer version for image pipeline

--- modules/optimizer/module.php
+++ modules/optimizer/module.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/Optimizer.php';
  */
 class Optimizer_Module extends Core_Module_Abstract
 {
-    public $version = '1.5.4';
+    public $version = '1.6.0';
     public $date = '2026-07-10';
 
     protected $_moduleName = 'optimizer';
